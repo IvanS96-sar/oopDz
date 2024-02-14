100c4b381e86dc1193f6db95707eb86aa767ea19
IvanS96-sar <ivan.shesterkin96@yandex.ru>
2024-02-14 21:36:59 +0300

    2.Исправленный вариант

--- oop/CarService.java
+++ oop/CarService.java
@@ -5,13 +5,11 @@ public class CarService {
         Bicycle bicycle = new Bicycle(2,32,"горный");
         Car car = new Car(4,224,"бензиновый","Ford","focus");
         Truck truck = new Truck(10,130,"дизельный",17000);
-        visitServiceStation(car,bicycle,truck);
+        visitServiceStation(bicycle);
+        visitServiceStation(car);
+        visitServiceStation(truck);
     }
-    public static void visitServiceStation(Car car,Bicycle bicycle,Truck truck){
-        car.service();
-        System.out.println();
-        bicycle.service();
-        System.out.println();
-        truck.service();
+    public static void visitServiceStation(WheeledTransport wheeledTransport){
+        wheeledTransport.service();
     }
 }

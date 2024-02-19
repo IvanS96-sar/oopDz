954504b67dafcd1e901195d28cbbc3820d1456cf
IvanS96-sar <ivan.shesterkin96@yandex.ru>
2024-02-19 09:31:41 +0300

    Добавил private

--- org/example/footballGame/Player.java
+++ org/example/footballGame/Player.java
@@ -4,7 +4,7 @@ public class Player {
     private int stamina;
     private static final int MAX_STAMINA = 100;
     private static final int MIN_STAMINA = 0;
-    static int countPlayers = 0;
+    private static int countPlayers = 0;
 
     public Player (int stamina) {
         this.stamina = stamina;
